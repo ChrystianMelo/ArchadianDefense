ChrystianMelo/ArchadianDefense

commit b216d4260f2d38a9bced3c7180e5d45a31542f9d

FIxed some issues.

tests/test_DFS.cpp
@@ -1,226 +1,0 @@
-#include <boost/test/unit_test.hpp>
-
-#include <unordered_set>
-
-#include "Archadian.h"
-#include "City.h"
-#include "Algorithms.h"
-
-namespace {
-	class VisitedNodeCollector : public NodeVisitor {
-	public:
-		explicit VisitedNodeCollector()
-			: visited_() {}
-
-		void visit(City* node) override {
-			visited_.insert(*node);
-		}
-
-		std::unordered_set<City> getVisited() {
-			return visited_;
-		}
-	private:
-		std::unordered_set<City> visited_;
-	};
-}
-
-// DFS visitas
-BOOST_AUTO_TEST_CASE(DFS_VisitsAllVertices) {
-	City node1(1), node2(2), node3(3), node4(4);
-
-	node1.connect(&node2);
-	node2.connect(&node3);
-	node3.connect(&node4);
-
-	Archadian Archadian({ node1,node2,node3,node4 });
-
-	VisitedNodeCollector visitedNodeCollector = VisitedNodeCollector();
-	auto dfs_data = Algorithms::DFS(&Archadian, &visitedNodeCollector);
-	std::unordered_set<City> visited = visitedNodeCollector.getVisited();
-
-	BOOST_CHECK(visited.find(node1) != visited.end());
-	BOOST_CHECK(visited.find(node2) != visited.end());
-	BOOST_CHECK(visited.find(node3) != visited.end());
-	BOOST_CHECK(visited.find(node4) != visited.end());
-
-	auto [colors, discovery_times, finishing_times] = dfs_data;
-
-	for (auto& node : { &node1, &node2, &node3, &node4 }) {
-		BOOST_CHECK(colors[node] == CityColor::FINISHED);
-	}
-
-	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_discovery_times = {
-		{ &node1, 1 },
-		{ &node2, 2 },
-		{ &node3, 3 },
-		{ &node4, 4 }
-	};
-
-	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_finishing_times = {
-		{ &node1, 8 },
-		{ &node2, 7 },
-		{ &node3, 6 },
-		{ &node4, 5 }
-	};
-
-	for (auto& node : { &node1, &node2, &node3, &node4 }) {
-		BOOST_CHECK(discovery_times.find(node) != discovery_times.end());
-		BOOST_CHECK(finishing_times.find(node) != finishing_times.end());
-
-		DiscoveryTime d_time = discovery_times[node];
-		FinishingTime f_time = finishing_times[node];
-
-		BOOST_CHECK(d_time < f_time);
-
-		BOOST_CHECK(d_time == expected_discovery_times[node]);
-		BOOST_CHECK(f_time == expected_finishing_times[node]);
-	}
-}
-
-// Teste DFS com um grafo mais complexo e verificação dos tempos esperados
-BOOST_AUTO_TEST_CASE(DFS_ComplexArchadianTraversal) {
-	City node1(1), node2(2), node3(3), node4(4), node5(5), node6(6), node7(7);
-
-	node1.connect(&node2);
-	node1.connect(&node3);
-	node2.connect(&node4);
-	node2.connect(&node5);
-	node3.connect(&node6);
-	node4.connect(&node7);
-	node5.connect(&node7);
-	node6.connect(&node7);
-	node7.connect(&node1);
-
-	Archadian Archadian({ node1, node2, node3, node4, node5, node6, node7 });
-
-	VisitedNodeCollector visitedNodeCollector = VisitedNodeCollector();
-	DFS_DATA dfs_data = Algorithms::DFS(&Archadian, &visitedNodeCollector);
-	std::unordered_set<City> visited = visitedNodeCollector.getVisited();
-
-	BOOST_CHECK(visited.find(node1) != visited.end());
-	BOOST_CHECK(visited.find(node2) != visited.end());
-	BOOST_CHECK(visited.find(node3) != visited.end());
-	BOOST_CHECK(visited.find(node4) != visited.end());
-	BOOST_CHECK(visited.find(node5) != visited.end());
-	BOOST_CHECK(visited.find(node6) != visited.end());
-	BOOST_CHECK(visited.find(node7) != visited.end());
-
-	auto [colors, discovery_times, finishing_times] = dfs_data;
-
-	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6, &node7 }) {
-		BOOST_CHECK(colors[node] == CityColor::FINISHED);
-	}
-
-	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_discovery_times = {
-		{ &node1, 1 },
-		{ &node2, 2 },
-		{ &node3, 10 },
-		{ &node4, 3 },
-		{ &node5, 7 },
-		{ &node6, 11 },
-		{ &node7, 4 }
-	};
-
-	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_finishing_times = {
-		{ &node1, 14 },
-		{ &node2, 9 },
-		{ &node3, 13 },
-		{ &node4, 6 },
-		{ &node5, 8 },
-		{ &node6, 12 },
-		{ &node7, 5 }
-	};
-
-	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6, &node7 }) {
-		// Cada nó deve ter tempos de descoberta e término definidos
-		BOOST_CHECK(discovery_times.find(node) != discovery_times.end());
-		BOOST_CHECK(finishing_times.find(node) != finishing_times.end());
-
-		DiscoveryTime d_time = discovery_times[node];
-		FinishingTime f_time = finishing_times[node];
-
-		BOOST_CHECK(d_time < f_time);
-
-		BOOST_CHECK(d_time == expected_discovery_times[node]);
-		BOOST_CHECK(f_time == expected_finishing_times[node]);
-	}
-}
-
-// Teste DFS em um grafo desconexo
-BOOST_AUTO_TEST_CASE(DFS_DisconnectedArchadianTest) {
-	// Criação dos nós para o primeiro componente conectado
-	City node1(1), node2(2), node3(3);
-
-	node1.connect(&node2);
-	node2.connect(&node3);
-
-	// Criação dos nós para o segundo componente conectado
-	City node4(4), node5(5);
-
-	node4.connect(&node5);
-
-	// Criação do nó isolado (terceiro componente)
-	City node6(6);
-
-	// Criação do grafo com todos os nós
-	Archadian Archadian({ node1, node2, node3, node4, node5, node6 });
-
-	// Execução do DFS e obtenção do retorno
-	VisitedNodeCollector visitedNodeCollector = VisitedNodeCollector();
-	DFS_DATA dfs_data = Algorithms::DFS(&Archadian, &visitedNodeCollector);
-	std::unordered_set<City> visited = visitedNodeCollector.getVisited();
-
-	// Verificação se todos os nós foram visitados
-	BOOST_CHECK(visited.find(node1) != visited.end());
-	BOOST_CHECK(visited.find(node2) != visited.end());
-	BOOST_CHECK(visited.find(node3) != visited.end());
-	BOOST_CHECK(visited.find(node4) != visited.end());
-	BOOST_CHECK(visited.find(node5) != visited.end());
-	BOOST_CHECK(visited.find(node6) != visited.end());
-
-	// Extração dos dados retornados pelo DFS
-	auto [colors, discovery_times, finishing_times] = dfs_data;
-
-	// Verificação das cores dos nós após o DFS
-	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6 }) {
-		// O nó deve estar marcado como "preto" após a conclusão do DFS
-		BOOST_CHECK(colors[node] == CityColor::FINISHED);
-	}
-
-	// Tempos esperados de descoberta e término para cada nó
-	// Supondo que o DFS explora os nós na ordem em que são fornecidos na lista de nós do grafo
-	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_discovery_times = {
-		{ &node1, 1 },
-		{ &node2, 2 },
-		{ &node3, 3 },
-		{ &node4, 7 },
-		{ &node5, 8 },
-		{ &node6, 11 }
-	};
-
-	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_finishing_times = {
-		{ &node1, 6 },
-		{ &node2, 5 },
-		{ &node3, 4 },
-		{ &node4, 10 },
-		{ &node5, 9 },
-		{ &node6, 12 }
-	};
-
-	// Verificação dos tempos de descoberta e término
-	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6 }) {
-		// Cada nó deve ter tempos de descoberta e término definidos
-		BOOST_CHECK(discovery_times.find(node) != discovery_times.end());
-		BOOST_CHECK(finishing_times.find(node) != finishing_times.end());
-
-		DiscoveryTime d_time = discovery_times[node];
-		FinishingTime f_time = finishing_times[node];
-
-		// O tempo de descoberta deve ser menor que o tempo de término
-		BOOST_CHECK(d_time < f_time);
-
-		// Os tempos devem corresponder aos tempos esperados
-		BOOST_CHECK(d_time == expected_discovery_times[node]);
-		BOOST_CHECK(f_time == expected_finishing_times[node]);
-	}
-}

tests/test_Dijkstra.cpp
@@ -1,90 +1,0 @@
-#include <boost/test/unit_test.hpp>
-
-#include "Archadian.h"
-#include "City.h"
-#include "Algorithms.h"
-
-// Teste do algoritmo de Dijkstra para calcular a distância mínima
-BOOST_AUTO_TEST_CASE(Dijkstra_ShortestPath) {
-	City node1(1), node2(2), node3(3), node4(4);
-
-	node1.connect(&node2);
-	node2.connect(&node3);
-	node3.connect(&node4);
-	node1.connect(&node3);
-	Archadian Archadian({ node1, node2, node3, node4 });
-
-	auto distances = Algorithms::Dijkstra(&Archadian, node1);
-
-	BOOST_CHECK(distances[&node1] == 0);
-	BOOST_CHECK(distances[&node2] == 1);
-	BOOST_CHECK(distances[&node3] == 1);
-	BOOST_CHECK(distances[&node4] == 2);
-}
-
-// Teste complexo do algoritmo de Dijkstra para calcular a distância mínima
-BOOST_AUTO_TEST_CASE( Dijkstra_ComplexShortestPath) {
-	City node1(1), node2(2), node3(3), node4(4), node5(5), node6(6);
-
-	node1.connect(&node2);
-	node1.connect(&node3);
-	node2.connect(&node3);
-	node2.connect(&node4);
-	node3.connect(&node5);
-	node5.connect(&node4);
-	node4.connect(&node6);
-	node5.connect(&node6);
-	node1.connect(&node6);
-
-	Archadian Archadian({ node1, node2, node3, node4, node5, node6 });
-
-	auto distances = Algorithms::Dijkstra(&Archadian, node1);
-
-	BOOST_CHECK(distances[&node1] == 0);
-	BOOST_CHECK(distances[&node2] == 1);
-	BOOST_CHECK(distances[&node3] == 1);
-	BOOST_CHECK(distances[&node4] == 2);
-	BOOST_CHECK(distances[&node5] == 2);
-	BOOST_CHECK(distances[&node6] == 1);
-}
-
-// Teste do algoritmo de Dijkstra em um grafo com ciclos
-BOOST_AUTO_TEST_CASE(Dijkstra_CycleArchadianTest) {
-	City node1(1), node2(2), node3(3), node4(4);
-
-	node1.connect(&node2);
-	node2.connect(&node3);
-	node3.connect(&node4);
-	node4.connect(&node2);
-	node3.connect(&node1);
-
-	Archadian Archadian({ node1, node2, node3, node4 });
-
-	auto distances = Algorithms::Dijkstra(&Archadian, node1);
-
-	BOOST_CHECK(distances[&node1] == 0);
-	BOOST_CHECK(distances[&node2] == 1);
-	BOOST_CHECK(distances[&node3] == 2);
-	BOOST_CHECK(distances[&node4] == 3);
-}
-
-// Teste do algoritmo de Dijkstra em um grafo desconexo
-BOOST_AUTO_TEST_CASE(Dijkstra_DisconnectedArchadianTest) {
-	City node1(1), node2(2), node3(3), node4(4), node5(5);
-
-	node1.connect(&node2);
-	node2.connect(&node3);
-
-	node4.connect(&node5);
-
-	Archadian Archadian({ node1, node2, node3, node4, node5 });
-
-	auto distances = Algorithms::Dijkstra(&Archadian, node1);
-
-	BOOST_CHECK(distances[&node1] == 0);
-	BOOST_CHECK(distances[&node2] == 1);
-	BOOST_CHECK(distances[&node3] == 2);
-
-	BOOST_CHECK(distances[&node4] == 999); //std::numeric_limits<int>::max());
-	BOOST_CHECK(distances[&node5] == 999); //std::numeric_limits<int>::max());
-}

commit 89c9c52e9c6567ef64fa94f2453721d0c64a073e

Fix: Undo unintended remotion caused by commit b216d42.

tests/test_DFS.cpp
@@ -1,0 +1,226 @@
+#include <boost/test/unit_test.hpp>
+
+#include <unordered_set>
+
+#include "Archadian.h"
+#include "City.h"
+#include "Algorithms.h"
+
+namespace {
+	class VisitedNodeCollector : public NodeVisitor {
+	public:
+		explicit VisitedNodeCollector()
+			: visited_() {}
+
+		void visit(City* node) override {
+			visited_.insert(*node);
+		}
+
+		std::unordered_set<City> getVisited() {
+			return visited_;
+		}
+	private:
+		std::unordered_set<City> visited_;
+	};
+}
+
+// DFS visitas
+BOOST_AUTO_TEST_CASE(DFS_VisitsAllVertices) {
+	City node1(1), node2(2), node3(3), node4(4);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+	node3.connect(&node4);
+
+	Archadian Archadian({ node1,node2,node3,node4 });
+
+	VisitedNodeCollector visitedNodeCollector = VisitedNodeCollector();
+	auto dfs_data = Algorithms::DFS(&Archadian, &visitedNodeCollector);
+	std::unordered_set<City> visited = visitedNodeCollector.getVisited();
+
+	BOOST_CHECK(visited.find(node1) != visited.end());
+	BOOST_CHECK(visited.find(node2) != visited.end());
+	BOOST_CHECK(visited.find(node3) != visited.end());
+	BOOST_CHECK(visited.find(node4) != visited.end());
+
+	auto [colors, discovery_times, finishing_times] = dfs_data;
+
+	for (auto& node : { &node1, &node2, &node3, &node4 }) {
+		BOOST_CHECK(colors[node] == CityColor::FINISHED);
+	}
+
+	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_discovery_times = {
+		{ &node1, 1 },
+		{ &node2, 2 },
+		{ &node3, 3 },
+		{ &node4, 4 }
+	};
+
+	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_finishing_times = {
+		{ &node1, 8 },
+		{ &node2, 7 },
+		{ &node3, 6 },
+		{ &node4, 5 }
+	};
+
+	for (auto& node : { &node1, &node2, &node3, &node4 }) {
+		BOOST_CHECK(discovery_times.find(node) != discovery_times.end());
+		BOOST_CHECK(finishing_times.find(node) != finishing_times.end());
+
+		DiscoveryTime d_time = discovery_times[node];
+		FinishingTime f_time = finishing_times[node];
+
+		BOOST_CHECK(d_time < f_time);
+
+		BOOST_CHECK(d_time == expected_discovery_times[node]);
+		BOOST_CHECK(f_time == expected_finishing_times[node]);
+	}
+}
+
+// Teste DFS com um grafo mais complexo e verificação dos tempos esperados
+BOOST_AUTO_TEST_CASE(DFS_ComplexArchadianTraversal) {
+	City node1(1), node2(2), node3(3), node4(4), node5(5), node6(6), node7(7);
+
+	node1.connect(&node2);
+	node1.connect(&node3);
+	node2.connect(&node4);
+	node2.connect(&node5);
+	node3.connect(&node6);
+	node4.connect(&node7);
+	node5.connect(&node7);
+	node6.connect(&node7);
+	node7.connect(&node1);
+
+	Archadian Archadian({ node1, node2, node3, node4, node5, node6, node7 });
+
+	VisitedNodeCollector visitedNodeCollector = VisitedNodeCollector();
+	DFS_DATA dfs_data = Algorithms::DFS(&Archadian, &visitedNodeCollector);
+	std::unordered_set<City> visited = visitedNodeCollector.getVisited();
+
+	BOOST_CHECK(visited.find(node1) != visited.end());
+	BOOST_CHECK(visited.find(node2) != visited.end());
+	BOOST_CHECK(visited.find(node3) != visited.end());
+	BOOST_CHECK(visited.find(node4) != visited.end());
+	BOOST_CHECK(visited.find(node5) != visited.end());
+	BOOST_CHECK(visited.find(node6) != visited.end());
+	BOOST_CHECK(visited.find(node7) != visited.end());
+
+	auto [colors, discovery_times, finishing_times] = dfs_data;
+
+	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6, &node7 }) {
+		BOOST_CHECK(colors[node] == CityColor::FINISHED);
+	}
+
+	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_discovery_times = {
+		{ &node1, 1 },
+		{ &node2, 2 },
+		{ &node3, 10 },
+		{ &node4, 3 },
+		{ &node5, 7 },
+		{ &node6, 11 },
+		{ &node7, 4 }
+	};
+
+	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_finishing_times = {
+		{ &node1, 14 },
+		{ &node2, 9 },
+		{ &node3, 13 },
+		{ &node4, 6 },
+		{ &node5, 8 },
+		{ &node6, 12 },
+		{ &node7, 5 }
+	};
+
+	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6, &node7 }) {
+		// Cada nó deve ter tempos de descoberta e término definidos
+		BOOST_CHECK(discovery_times.find(node) != discovery_times.end());
+		BOOST_CHECK(finishing_times.find(node) != finishing_times.end());
+
+		DiscoveryTime d_time = discovery_times[node];
+		FinishingTime f_time = finishing_times[node];
+
+		BOOST_CHECK(d_time < f_time);
+
+		BOOST_CHECK(d_time == expected_discovery_times[node]);
+		BOOST_CHECK(f_time == expected_finishing_times[node]);
+	}
+}
+
+// Teste DFS em um grafo desconexo
+BOOST_AUTO_TEST_CASE(DFS_DisconnectedArchadianTest) {
+	// Criação dos nós para o primeiro componente conectado
+	City node1(1), node2(2), node3(3);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+
+	// Criação dos nós para o segundo componente conectado
+	City node4(4), node5(5);
+
+	node4.connect(&node5);
+
+	// Criação do nó isolado (terceiro componente)
+	City node6(6);
+
+	// Criação do grafo com todos os nós
+	Archadian Archadian({ node1, node2, node3, node4, node5, node6 });
+
+	// Execução do DFS e obtenção do retorno
+	VisitedNodeCollector visitedNodeCollector = VisitedNodeCollector();
+	DFS_DATA dfs_data = Algorithms::DFS(&Archadian, &visitedNodeCollector);
+	std::unordered_set<City> visited = visitedNodeCollector.getVisited();
+
+	// Verificação se todos os nós foram visitados
+	BOOST_CHECK(visited.find(node1) != visited.end());
+	BOOST_CHECK(visited.find(node2) != visited.end());
+	BOOST_CHECK(visited.find(node3) != visited.end());
+	BOOST_CHECK(visited.find(node4) != visited.end());
+	BOOST_CHECK(visited.find(node5) != visited.end());
+	BOOST_CHECK(visited.find(node6) != visited.end());
+
+	// Extração dos dados retornados pelo DFS
+	auto [colors, discovery_times, finishing_times] = dfs_data;
+
+	// Verificação das cores dos nós após o DFS
+	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6 }) {
+		// O nó deve estar marcado como "preto" após a conclusão do DFS
+		BOOST_CHECK(colors[node] == CityColor::FINISHED);
+	}
+
+	// Tempos esperados de descoberta e término para cada nó
+	// Supondo que o DFS explora os nós na ordem em que são fornecidos na lista de nós do grafo
+	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_discovery_times = {
+		{ &node1, 1 },
+		{ &node2, 2 },
+		{ &node3, 3 },
+		{ &node4, 7 },
+		{ &node5, 8 },
+		{ &node6, 11 }
+	};
+
+	std::unordered_map<City*, std::size_t, CityHash, CityEqual> expected_finishing_times = {
+		{ &node1, 6 },
+		{ &node2, 5 },
+		{ &node3, 4 },
+		{ &node4, 10 },
+		{ &node5, 9 },
+		{ &node6, 12 }
+	};
+
+	// Verificação dos tempos de descoberta e término
+	for (auto& node : { &node1, &node2, &node3, &node4, &node5, &node6 }) {
+		// Cada nó deve ter tempos de descoberta e término definidos
+		BOOST_CHECK(discovery_times.find(node) != discovery_times.end());
+		BOOST_CHECK(finishing_times.find(node) != finishing_times.end());
+
+		DiscoveryTime d_time = discovery_times[node];
+		FinishingTime f_time = finishing_times[node];
+
+		// O tempo de descoberta deve ser menor que o tempo de término
+		BOOST_CHECK(d_time < f_time);
+
+		// Os tempos devem corresponder aos tempos esperados
+		BOOST_CHECK(d_time == expected_discovery_times[node]);
+		BOOST_CHECK(f_time == expected_finishing_times[node]);
+	}
+}

tests/test_Dijkstra.cpp
@@ -1,0 +1,90 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Archadian.h"
+#include "City.h"
+#include "Algorithms.h"
+
+// Teste do algoritmo de Dijkstra para calcular a distância mínima
+BOOST_AUTO_TEST_CASE(Dijkstra_ShortestPath) {
+	City node1(1), node2(2), node3(3), node4(4);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+	node3.connect(&node4);
+	node1.connect(&node3);
+	Archadian Archadian({ node1, node2, node3, node4 });
+
+	auto distances = Algorithms::Dijkstra(&Archadian, node1);
+
+	BOOST_CHECK(distances[&node1] == 0);
+	BOOST_CHECK(distances[&node2] == 1);
+	BOOST_CHECK(distances[&node3] == 1);
+	BOOST_CHECK(distances[&node4] == 2);
+}
+
+// Teste complexo do algoritmo de Dijkstra para calcular a distância mínima
+BOOST_AUTO_TEST_CASE( Dijkstra_ComplexShortestPath) {
+	City node1(1), node2(2), node3(3), node4(4), node5(5), node6(6);
+
+	node1.connect(&node2);
+	node1.connect(&node3);
+	node2.connect(&node3);
+	node2.connect(&node4);
+	node3.connect(&node5);
+	node5.connect(&node4);
+	node4.connect(&node6);
+	node5.connect(&node6);
+	node1.connect(&node6);
+
+	Archadian Archadian({ node1, node2, node3, node4, node5, node6 });
+
+	auto distances = Algorithms::Dijkstra(&Archadian, node1);
+
+	BOOST_CHECK(distances[&node1] == 0);
+	BOOST_CHECK(distances[&node2] == 1);
+	BOOST_CHECK(distances[&node3] == 1);
+	BOOST_CHECK(distances[&node4] == 2);
+	BOOST_CHECK(distances[&node5] == 2);
+	BOOST_CHECK(distances[&node6] == 1);
+}
+
+// Teste do algoritmo de Dijkstra em um grafo com ciclos
+BOOST_AUTO_TEST_CASE(Dijkstra_CycleArchadianTest) {
+	City node1(1), node2(2), node3(3), node4(4);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+	node3.connect(&node4);
+	node4.connect(&node2);
+	node3.connect(&node1);
+
+	Archadian Archadian({ node1, node2, node3, node4 });
+
+	auto distances = Algorithms::Dijkstra(&Archadian, node1);
+
+	BOOST_CHECK(distances[&node1] == 0);
+	BOOST_CHECK(distances[&node2] == 1);
+	BOOST_CHECK(distances[&node3] == 2);
+	BOOST_CHECK(distances[&node4] == 3);
+}
+
+// Teste do algoritmo de Dijkstra em um grafo desconexo
+BOOST_AUTO_TEST_CASE(Dijkstra_DisconnectedArchadianTest) {
+	City node1(1), node2(2), node3(3), node4(4), node5(5);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+
+	node4.connect(&node5);
+
+	Archadian Archadian({ node1, node2, node3, node4, node5 });
+
+	auto distances = Algorithms::Dijkstra(&Archadian, node1);
+
+	BOOST_CHECK(distances[&node1] == 0);
+	BOOST_CHECK(distances[&node2] == 1);
+	BOOST_CHECK(distances[&node3] == 2);
+
+	BOOST_CHECK(distances[&node4] == 999); //std::numeric_limits<int>::max());
+	BOOST_CHECK(distances[&node5] == 999); //std::numeric_limits<int>::max());
+}

tests/test_Kosaraju.cpp
@@ -1,0 +1,203 @@
+#include <boost/test/unit_test.hpp>
+
+#include <unordered_set>
+
+#include "Archadian.h"
+#include "City.h"
+#include "Algorithms.h"
+
+// Teste simplificado da função Kosaraju para encontrar componentes fortemente conectadas
+BOOST_AUTO_TEST_CASE(Kosaraju_Simplified) {
+	City node1(1), node2(2), node3(3);
+
+	node1.connect(&node2);
+	node2.connect(&node1);
+
+	node3.connect(&node2);
+
+	Archadian Archadian({ node1, node2, node3 });
+
+	std::vector<SCC> sccs = Algorithms::Kosaraju(&Archadian);
+
+	std::vector<std::unordered_set<std::size_t>> foundSCCs;
+	for (const auto& scc : sccs) {
+		std::unordered_set<std::size_t> scc_indices;
+		for (const auto& node : scc) {
+			scc_indices.insert(node.getIndex());
+		}
+		foundSCCs.push_back(scc_indices);
+	}
+
+	std::vector<std::unordered_set<std::size_t>> expectedSCCs = {
+		{3},       		{1, 2} };
+
+	BOOST_CHECK(foundSCCs.size() == expectedSCCs.size());
+
+	for (const auto& expected_scc : expectedSCCs) {
+		bool scc_found = false;
+		for (const auto& found_scc : foundSCCs) {
+			if (found_scc == expected_scc) {
+				scc_found = true;
+				break;
+			}
+		}
+		BOOST_CHECK(scc_found);
+	}
+}
+
+// Teste da função Kosaraju para encontrar componentes fortemente conectadas
+BOOST_AUTO_TEST_CASE(Kosaraju) {
+	City node1(1), node2(2), node3(3), node4(4), node5(5);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+	node3.connect(&node1);
+
+	node2.connect(&node4);
+
+	node4.connect(&node5);
+	node5.connect(&node4);
+
+	Archadian Archadian({ node1, node2, node3, node4, node5 });
+
+	std::vector<SCC> sccs = Algorithms::Kosaraju(&Archadian);
+
+	std::vector<std::unordered_set<std::size_t>> foundSCCs;
+	for (const auto& scc : sccs) {
+		std::unordered_set<std::size_t> scc_indices;
+		for (const auto& node : scc) {
+			scc_indices.insert(node.getIndex());
+		}
+		foundSCCs.push_back(scc_indices);
+	}
+
+	std::vector<std::unordered_set<std::size_t>> expectedSCCs = {
+		{1, 3,2},       		{4, 5} };
+
+	BOOST_CHECK(foundSCCs.size() == expectedSCCs.size());
+
+	for (const auto& expected_scc : expectedSCCs) {
+		bool scc_found = false;
+		for (const auto& found_scc : foundSCCs) {
+			if (found_scc == expected_scc) {
+				scc_found = true;
+				break;
+			}
+		}
+		BOOST_CHECK(scc_found);
+	}
+}
+
+// Teste 1: Nós Isolados
+BOOST_AUTO_TEST_CASE(Kosaraju_IsolatedNodesTest) {
+	City node1(1), node2(2), node3(3);
+
+	City node4(4), node5(5);
+	node4.connect(&node5);
+	node5.connect(&node4);
+
+	Archadian Archadian({ node1, node2, node3, node4, node5 });
+
+	std::vector<SCC> sccs = Algorithms::Kosaraju(&Archadian);
+
+	std::vector<std::unordered_set<std::size_t>> foundSCCs;
+	for (const auto& scc : sccs) {
+		std::unordered_set<std::size_t> scc_indices;
+		for (const auto& node : scc) {
+			scc_indices.insert(node.getIndex());
+		}
+		foundSCCs.push_back(scc_indices);
+	}
+
+	std::vector<std::unordered_set<std::size_t>> expectedSCCs = {
+		{1},       		{2},       		{3},       		{4, 5} };
+
+	BOOST_CHECK(foundSCCs.size() == expectedSCCs.size());
+
+	for (const auto& expected_scc : expectedSCCs) {
+		bool scc_found = false;
+		for (const auto& found_scc : foundSCCs) {
+			if (found_scc == expected_scc) {
+				scc_found = true;
+				break;
+			}
+		}
+		BOOST_CHECK(scc_found);
+	}
+}
+
+// Teste 2: Grafo Totalmente Conectado
+BOOST_AUTO_TEST_CASE(Kosaraju_FullyConnectedArchadianTest) {
+	City node1(1), node2(2), node3(3), node4(4);
+
+	node1.connect(&node2);
+	node1.connect(&node3);
+	node1.connect(&node4);
+
+	node2.connect(&node1);
+	node2.connect(&node3);
+	node2.connect(&node4);
+
+	node3.connect(&node1);
+	node3.connect(&node2);
+	node3.connect(&node4);
+
+	node4.connect(&node1);
+	node4.connect(&node2);
+	node4.connect(&node3);
+
+	Archadian Archadian({ node1, node2, node3, node4 });
+
+	std::vector<SCC> sccs = Algorithms::Kosaraju(&Archadian);
+
+	std::unordered_set<std::size_t> foundSCC;
+	for (const auto& scc : sccs) {
+		for (const auto& node : scc) {
+			foundSCC.insert(node.getIndex());
+		}
+	}
+
+	std::unordered_set<std::size_t> expectedSCC = { 1, 2, 3, 4 };
+
+	BOOST_CHECK(sccs.size() == 1);
+
+	BOOST_CHECK(foundSCC == expectedSCC);
+}
+
+// Teste 3: Grafo com Várias SCCs Individuais
+BOOST_AUTO_TEST_CASE(Kosaraju_MultipleSingleNodeSCCsTest) {
+	City node1(1), node2(2), node3(3), node4(4);
+
+	Archadian Archadian({ node1, node2, node3, node4 });
+
+	std::vector<SCC> sccs = Algorithms::Kosaraju(&Archadian);
+
+	std::vector<std::unordered_set<std::size_t>> foundSCCs;
+	for (const auto& scc : sccs) {
+		std::unordered_set<std::size_t> scc_indices;
+		for (const auto& node : scc) {
+			scc_indices.insert(node.getIndex());
+		}
+		foundSCCs.push_back(scc_indices);
+	}
+
+	std::vector<std::unordered_set<std::size_t>> expectedSCCs = {
+		{1},
+		{2},
+		{3},
+		{4}
+	};
+
+	BOOST_CHECK(foundSCCs.size() == expectedSCCs.size());
+
+	for (const auto& expected_scc : expectedSCCs) {
+		bool scc_found = false;
+		for (const auto& found_scc : foundSCCs) {
+			if (found_scc == expected_scc) {
+				scc_found = true;
+				break;
+			}
+		}
+		BOOST_CHECK(scc_found);
+	}
+}

tests/test_TrasnposeGraph.cpp
@@ -1,0 +1,97 @@
+#include <boost/test/unit_test.hpp>
+
+#include "Archadian.h"
+#include "City.h"
+#include "Algorithms.h"
+
+// Teste da função transposeArchadian para transpor um grafo com 3 nós
+BOOST_AUTO_TEST_CASE(TransposeArchadianWithThreeNodes) {
+	City node1(1), node2(2), node3(3);
+
+	node1.connect(&node2);
+	node2.connect(&node3);
+
+	Archadian Archadian({ node1, node2, node3 });
+
+	Algorithms::transposeArchadian(Archadian);
+
+	auto nodes = Archadian.getNodes();
+	for (City& node : nodes) {
+		if (node == nodes[0])
+			BOOST_CHECK(node.getEdges().size() == 0);
+		if (node == nodes[1]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[0]));
+		}
+		if (node == nodes[2]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[1]));
+		}
+
+	}
+
+	Algorithms::transposeArchadian(Archadian);
+
+	nodes = Archadian.getNodes();
+	for (City& node : nodes) {
+		if (node == nodes[0]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[1]));
+		}
+		if (node == nodes[1]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[2]));
+		}
+		if (node == nodes[2]) {
+			BOOST_CHECK(node.getEdges().size() == 0);
+		}
+	}
+}
+
+// Teste da função transposeArchadian para transpor um grafo com 3 nós
+BOOST_AUTO_TEST_CASE(TransposeArchadianWithThreeNodes_Simplified) {
+	City node1(1), node2(2), node3(3);
+
+	node1.connect(&node2);
+	node2.connect(&node1);
+
+	node3.connect(&node2);
+
+	Archadian Archadian({ node1, node2, node3 });
+
+	Algorithms::transposeArchadian(Archadian);
+
+	auto nodes = Archadian.getNodes();
+	for (City& node : nodes) {
+		if (node == nodes[0]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[1]));
+		}
+		if (node == nodes[1]) {
+			BOOST_CHECK(node.getEdges().size() == 2);
+			BOOST_CHECK(node.isConnected(&nodes[0]));
+			BOOST_CHECK(node.isConnected(&nodes[2]));
+		}
+		if (node == nodes[2]) {
+			BOOST_CHECK(node.getEdges().size() == 0);
+		}
+	}
+
+	Algorithms::transposeArchadian(Archadian);
+
+	nodes = Archadian.getNodes();
+	for (City& node : nodes) {
+		if (node == nodes[0]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[1]));
+		}
+		if (node == nodes[1]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[0]));
+		}
+		if (node == nodes[2]) {
+			BOOST_CHECK(node.getEdges().size() == 1);
+			BOOST_CHECK(node.isConnected(&nodes[1]));
+		}
+	}
+}